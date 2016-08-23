17a13adc3c95da7a18dcbef54ad3a8e2136632f7
glashunti <gleibsonglas@gmail.com>
2016-08-22 23:01:29 -0300

    Interaction
    Closing the windom pressing X on the display and controling the racket with the keybord

--- bola.py
+++ bola.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
 from time import sleep
 import pygame
+from pygame.locals import *
 pygame.init()
 
 screen = width, heigth = 800, 600
@@ -10,6 +12,7 @@ speed = [1,1]
 gravity = 1
 black = 0,0,0
 scenario = pygame.display.set_mode(screen)
+racket_center = [400,300]
 
 ball = pygame.image.load("bola.png")
 ballrect = ball.get_rect()
@@ -18,8 +21,10 @@ racket = pygame.image.load("raquete.png")
 racketrect = racket.get_rect()
 racketrect.center = (400, 300)
 
-
 while 1:
+   for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        sys.exit()
    if ballrect.bottom > heigth:
        ballrect.bottom = heigth
    ballrect = ballrect.move(speed)
@@ -40,7 +45,12 @@ while 1:
    speed[1] = speed[1] + gravity
 #   print 'ballrect.left' = + str(ballrect.left) + ' |ballrect.right = ' + str(ballrect.right) + '|ballrect.bottom = ' + str(ballrect.bottom)
 #   print 'racketrect.left' = + str(racketrect.left) + '|racketrect.right = ' + str(racketrect.right) + '|racketrect.top = ' + str(racketrect.top)
-   
+   buton = pygame.key.get_pressed()
+   if (buton[K_LEFT]):
+      racket_center[0] = racket_center[0] - 1
+   elif (buton[K_RIGHT]):
+      racket_center[0] = racket_center[0] + 1
+   racketrect.center = (racket_center[0], racket_center[1])
    scenario.fill(black)
    scenario.blit(ball, ballrect)
    scenario.blit(racket, racketrect)
