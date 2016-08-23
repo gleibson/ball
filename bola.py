d9b9ec9283ef3ba1a18bf2ac3d4d6ee458083f4d
glashunti <gleibsonglas@gmail.com>
2016-08-22 22:35:27 -0300

    Adding the racket
    Adding the racket to work with the ball

--- bola.py
+++ bola.py
@@ -14,23 +14,39 @@ scenario = pygame.display.set_mode(screen)
 ball = pygame.image.load("bola.png")
 ballrect = ball.get_rect()
 
+racket = pygame.image.load("raquete.png")
+racketrect = racket.get_rect()
+racketrect.center = (400, 300)
+
+
 while 1:
    if ballrect.bottom > heigth:
        ballrect.bottom = heigth
    if ballrect.right > width:
        ballrect.right = width
    ballrect = ballrect.move(speed)
-   if ballrect.right < 0 or ballrect.left > width:
-       speed[0] = -speed[0]
-   if ballrect.top < 0 or ballrect.bottom > heigth:
-       speed[1] = -speed[1]
-   if speed[1] >= 0:
-       speed[1] = speed[1] + gravity
-   else: 
-       speed[1] = speed[1] + 1.1*gravity
+   if ballrect.bottom >= racketrect.top and ballrect.right >= racketrect.left and ballrect.left <= racketrect.right:
+      speed[1] = -speed[1]
+      speed[1] = speed[1] - 1.5*gravity
+      raw_input("Press Enter to continue...")
+   
+   else:
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > heigth:
+            speed[1] = -speed[1]
+        
+        if speed[1] >= 0:
+            speed[1] = speed[1] + gravity
+        else: 
+            speed[1] = speed[1] + 1.1*gravity
    speed[1] = speed[1] + gravity
-   print 'speed[1]: ' + str(speed[1]) + ' ballrect.bottom: ' + str(ballrect.bottom)
+#   print 'ballrect.left' = + str(ballrect.left) + ' |ballrect.right = ' + str(ballrect.right) + '|ballrect.bottom = ' + str(ballrect.bottom)
+#   print 'racketrect.left' = + str(racketrect.left) + '|racketrect.right = ' + str(racketrect.right) + '|racketrect.top = ' + str(racketrect.top)
+   
    scenario.fill(black)
    scenario.blit(ball, ballrect)
+   scenario.blit(racket, racketrect)
    pygame.display.flip()
    sleep(0.02)
+#        raw_input("Press Enter to continue...")
